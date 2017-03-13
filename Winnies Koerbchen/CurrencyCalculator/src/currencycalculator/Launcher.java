16eb4fea2d30ce35e790068fe03766680f3b1842
Leistergitter <fabian.winnebald@mni.thm.de>
2017-03-13 13:33:28 +0100

    Create Launcher.java

---
+++ Winnies Koerbchen/CurrencyCalculator/src/currencycalculator/Launcher.java
@@ -0,0 +1,10 @@
+
+package currencycalculator;
+
+import javafx.application.Application;
+
+public class Launcher {
+        public static void main(String[] args) {
+            Application.launch(CurrencyCalculator.class, args);
+        } 
+}
