17d34221f40015a5eff9723197e09509136f0797
lain4 <lain4@users.noreply.github.com>
2017-03-20 12:01:36 +0100

    Update GameState.java

--- BierChallenge/src/GameState.java
+++ BierChallenge/src/GameState.java
@@ -3,14 +3,14 @@ package beerchallenge;
 import java.util.LinkedList;
 import javafx.beans.property.IntegerProperty;
 import javafx.beans.property.SimpleIntegerProperty;
+import javafx.scene.Parent;
 
 
 public class GameState {
-            private final static Game[] GAMES = new Game[7];
             private LinkedList<Team> teams;
             private Team team1, team2;
                        
-            static Game game;
+            private Game game;
             
             GameState() {
                 this.teams = null;
@@ -27,14 +27,40 @@ public class GameState {
                 this.teams = teams;
                 this.team1 = teams.get(t1);
                 this.team2 = teams.get(t2);
-                game = GAMES[index];
+                game = null;
                 game.setTurn(turn);
                 game.setScore1(sc1);
                 game.setScore2(sc2);  
             }
             
+            final LinkedList<Team> getTeams() {
+                return teams;
+            }
+            
+            final Game currentGame() {
+                return game;
+            }
+            
+            private Game getGame(int index) {            
+                switch(index) {
+                    case 0:
+                        game = new Pong(team1, team2);
+                        return game;
+                    default:
+                        return new Pong(team1, team2);
+                }
+            }
+            
+            final Parent showGame(int index) {
+                return getGame(index).showContent();
+            }
+            
             public final Team getTeam1() {
                 return team1;
+            }
+            
+            public final void addTeam(Team team) {
+                teams.add(team);
             }
             
             public final Team getTeam2() {
