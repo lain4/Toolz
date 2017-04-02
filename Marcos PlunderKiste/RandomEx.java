efff639a7326e8aa943936210acd0d4eed0573b8
lain4 <lain4@users.noreply.github.com>
2017-04-02 11:45:56 +0200

    overloaded method
    - more comfort
    
    to-do:
    - input-checks

--- Marcos PlunderKiste/RandomEx.java
+++ Marcos PlunderKiste/RandomEx.java
@@ -3,8 +3,12 @@ import java.util.ArrayList;
 import java.util.stream.IntStream;
 
 public class RandomEx extends java.util.Random {
+    
+    final int nextInt(int range, int... ex) {
+        return nextInt(0, range, ex);
+    }
 
-    public final int nextInt(int start, int end, int... ex) {
+    final int nextInt(int start, int end, int... ex) {
         int[] exs = ex;
         List<Integer> range = new ArrayList<>();
         IntStream.range(start, end + 1).forEach(e -> range.add(e));
