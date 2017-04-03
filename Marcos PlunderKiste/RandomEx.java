0bd6b17325f5eb3894f47b08cf1e069f49cfc3a5
lain4 <lain4@users.noreply.github.com>
2017-04-03 12:01:28 +0200

    Update RandomEx.java

--- Marcos PlunderKiste/RandomEx.java
+++ Marcos PlunderKiste/RandomEx.java
@@ -1,8 +1,18 @@
 import java.util.List;
 import java.util.ArrayList;
 import java.util.stream.IntStream;
+import java.util.Collections;
 
 public class RandomEx extends java.util.Random {
+    
+    final int nextInt(int start, int end, List<Integer> exs) {
+        List<Integer> range = new ArrayList<>();
+        IntStream.range(start, end + 1)
+                 .filter(i -> !exs.contains(i))
+                 .forEach(e -> range.add(e));
+        Collections.shuffle(range);
+        return range.get(0);
+    }
 
     final int nextInt(int start, int end, int... ex) {
         int[] exs = ex;
@@ -11,7 +21,7 @@ public class RandomEx extends java.util.Random {
         for(Integer x : exs) {
             range.remove(x);
         }
-        java.util.Collections.shuffle(range);
+        Collections.shuffle(range);
         return range.get(0);
     }
 
